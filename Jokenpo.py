cd8ae51147d310db4ec3f7c8f9979e7fe8bafebe
SimoneBraghin <braghin.aai@gmail.com>
2024-01-24 21:57:20 -0300

    jogo MVP 1 finalizado. Aguardando testes e sugestões de melhoria

--- Jokenpo.py
+++ Jokenpo.py
@@ -1,61 +1,69 @@
-"""" RASCUNHO DAS REGRAS (PRA FACILITAR A CODAGEM, DEPOIS EXCLUIMOS NO FINAL ;) )
-- Pedra ganha da tesoura (amassando-a ou quebrando-a).
-- Tesoura ganha do papel (cortando-o).
-- Papel ganha da pedra (embrulhando-a).
-
-A pedra é simbolizada por um punho fechado; 
-a tesoura, por dois dedos esticados; 
-e o papel, pela mão aberta. 
-Caso dois jogadores façam o mesmo gesto, ocorre um empate, 
-e geralmente se joga de novo até desempatar. 
-*Este jogo possui uma única regra: não é permitido mostrar pedra duas vezes seguidas*.
-"""
-
 import random
 import time
 
 print("--------- Bem vindo ao Jokenpô --------- \n!!!(じゃんけんぽん いらっしゃいませ)!!!")
 #print() >>> explicar as regras básicas (um resumo do arquivo de regras do jogo)
 
+
 scoreJogador = 0
 scoreBot = 0
-partidaFinalizada = False
 
-#while partidaFinalizada == False:
-configuracaoMao = {
-    0 : "Pedra",
-    1 : "Papel",
-    2 : "Tesoura" 
-}
+while scoreJogador <3 or scoreBot <3:
+  
+    configuracaoMao = {
+        0 : "Pedra",
+        1 : "Papel",
+        2 : "Tesoura" 
+    }
 
-botRandom = random.randint(0,2)
-#jogadorBotMao = configuracaoMao[botRandom]
-print(type(botRandom))
-#print(type(jogadorBotMao))
+    botRandom = random.randint(0,2)
+    escolhaJogador = int(input("Informe sua configuração de mão: \n[0] Pedra \n[1] Papel \n[2] Tesoura \n>>> "))
+       
+    time.sleep(1)
+    print("Jan")
+    time.sleep(1)
+    print("Ken")
+    time.sleep(1)
+    print("Pô!\n")
+    print("Você jogou: "+ configuracaoMao[escolhaJogador])
+    print("Seu oponente jogou "+ configuracaoMao[botRandom])
 
+    if escolhaJogador == botRandom: # se impatou repete round (mesma configuração)
+        print("Empate. Vamos de novo!")
 
-escolhaJogador = int(input("Informe sua configuração de mão: \n[0] Pedra \n[1] Papel \n[2] Tesoura \n>>> "))
-print(type(escolhaJogador))
+    elif escolhaJogador == 0 and botRandom == 1:
+        print("Você perdeu a mão")
+        scoreBot += 1
+        print(f"Seu saldo de vitórias: {scoreJogador} \nSaldo de vitórias do seu adversário: {scoreBot}")
+    elif escolhaJogador == 0 and botRandom == 2: 
+        print("Você ganhou a mão")
+        scoreJogador += 1
+        print(f"Seu saldo de vitórias: {scoreJogador} \nSaldo de vitórias do seu adversário: {scoreBot}")
 
-time.sleep(1)
-print("Jan")
-time.sleep(1)
-print("Ken")
-time.sleep(1)
-print("Pô!\n")
-print("Você jogou: "+ configuracaoMao[escolhaJogador])
-print("Seu oponente jogou "+ configuracaoMao[botRandom])
+    elif escolhaJogador == 1 and botRandom == 2:
+        print("Você perdeu a mão")
+        scoreBot += 1
+        print(f"Seu saldo de vitórias: {scoreJogador} \nSaldo de vitórias do seu adversário: {scoreBot}")
+    elif escolhaJogador == 1 and botRandom == 0: 
+        print("Você ganhou a mão")
+        scoreJogador += 1
+        print(f"Seu saldo de vitórias: {scoreJogador} \nSaldo de vitórias do seu adversário: {scoreBot}")
 
-if botRandom == escolhaJogador: # se impatou repete round (mesma configuração)
-    print("Empate. Vamos de novo!")
-
-#Quem ganho a mão Acrua +1
-# precisa construir as condicionais
+    elif escolhaJogador == 2 and botRandom == 0:
+        print("Você perdeu a mão")
+        scoreBot += 1
+        print(f"Seu saldo de vitórias: {scoreJogador} \nSaldo de vitórias do seu adversário: {scoreBot}")
+    elif escolhaJogador == 2 and botRandom == 1: 
+        print("Você ganhou a mão")
+        scoreJogador += 1
+        print(f"Seu saldo de vitórias: {scoreJogador} \nSaldo de vitórias do seu adversário: {scoreBot}")
     
+    if scoreBot == 3:
+        print("Você perdeu o jogo! \nTente outra vez!")
+        break
+    if scoreJogador == 3:
+        print("P-A-R-A-B-É-N-S!!!\nVocê GANHOU o jogo! \nTente outra vez! Quem sabe nessa eu ganho de você ;) ")
+        break
+        
 
-# se input atual máquina ou jogador  == input máquina ou jogador anterior não permitir jogada
-# *Este jogo possui uma única regra: não é permitido mostrar pedra duas vezes seguidas*
-
-
-#Daqui pra baixo tá em contrução - parte de cima tá 100% funcionando
-# repetir jogo até vitorioso +3 placar
+# Próximo MVP: *Este jogo possui uma única regra: não é permitido mostrar pedra duas vezes seguidas*
